6b86b0ae0c4677ff2b67345b72e8a15fa3dbebbd
Kyle E. Mitchell <kyle@kemitchell.com>
2021-04-23 17:51:15 -0700

    Rewrite as reverse Pinboard reader

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const log = require('pino')()
 
-let lastUpdated = null
-
 process.on('SIGINT', trap)
 process.on('SIGQUIT', trap)
 process.on('SIGTERM', trap)
@@ -23,13 +21,23 @@ function close () {
   })
 }
 
-const TITLE = process.env.TITLE || 'To-Do List'
+const TITLE = process.env.TITLE || 'Reverse Pinboard'
 
-const REPOSITORY = process.env.REPOSITORY
-if (!REPOSITORY) {
-  log.error('no REPOSITORY in env')
+const PINBOARD_TOKEN = process.env.PINBOARD_TOKEN
+if (!PINBOARD_TOKEN) {
+  log.error('no PINBOARD_TOKEN in env')
   process.exit(1)
 }
+
+const DIRECTORY = process.env.DIRECTORY
+if (!DIRECTORY) {
+  log.error('no DIRECTORY in env')
+  process.exit(1)
+}
+
+const path = require('path')
+const POSTS_FILE = path.join(DIRECTORY, 'posts.json')
+const UPDATED_FILE = path.join(DIRECTORY, 'updated')
 
 const USERNAME = process.env.USERNAME
 if (!USERNAME) {
@@ -43,20 +51,12 @@ if (!PASSWORD) {
   process.exit(1)
 }
 
-const path = require('path')
 const addLogs = require('pino-http')({ logger: log })
-const refreshPath = '/refresh'
 const server = require('http').createServer((request, response) => {
   addLogs(request, response)
   const method = request.method
   if (method === 'GET') return get(request, response)
-  if (method === 'POST') {
-    if (request.url === refreshPath) {
-      return refresh(request, response)
-    } else {
-      return post(request, response)
-    }
-  }
+  if (method === 'POST') return post(request, response)
   response.statusCode = 405
   response.end()
 })
@@ -65,10 +65,6 @@ const basicAuth = require('basic-auth')
 const escapeHTML = require('escape-html')
 const fs = require('fs')
 const runParallel = require('run-parallel')
-const moment = require('moment-timezone')
-const linkifyURLs = require('linkify-urls')
-
-const TZ = 'America/Los_Angeles'
 
 function get (request, response) {
   const auth = basicAuth(request)
@@ -77,28 +73,20 @@ function get (request, response) {
     response.setHeader('WWW-Authenticate', 'Basic realm=todo')
     return response.end()
   }
-  fs.readdir(REPOSITORY, (error, entries) => {
+  fs.readFile(POSTS_FILE, (error, json) => {
     if (error) return internalError(error)
-    const withDueDate = entries
-      .filter(entry => {
-        return entry !== 'sort' && !entry.startsWith('.')
-      })
-      .map(entry => done => { processFile(entry, done) })
-    runParallel(withDueDate, (error, results) => {
-      if (error) return internalError(error)
-      const todos = results
-        .reduce((items, array) => items.concat(array))
-        .sort((a, b) => {
-          if (a.dateString && b.dateString) {
-            return compareDateStrings(a.dateString, b.dateString)
-          } else if (a.dateString) {
-            return -1
-          } else {
-            return 1
-          }
-        })
-      render(todos)
-    })
+    let posts
+    try {
+      posts = JSON.parse(json)
+    } catch (error) {
+      return internalError(error)
+    }
+    render(
+      posts
+        .filter(item => item.toread === 'yes')
+        .sort((a, b) => a.time.localeCompare(b.time))
+        .slice(0, 100)
+    )
   })
 
   function internalError (error) {
@@ -107,27 +95,7 @@ function get (request, response) {
     response.end()
   }
 
-  function render (todos) {
-    const withDueDate = []
-    const todayMoment = moment()
-    const dueToday = []
-    const ongoing = []
-    const basenames = new Set()
-    todos.forEach(todo => {
-      basenames.add(todo.basename)
-      if (todo.dateString) {
-        const todoMoment = moment.tz(todo.dateString, TZ)
-        withDueDate.push(todo)
-        if (todoMoment.isSame(todayMoment, 'day')) {
-          todo.today = true
-          dueToday.push(todo)
-        }
-      } else ongoing.push(todo)
-    })
-    ongoing.sort((a, b) => a.basename.localeCompare(b.basename))
-    const options = Array.from(basenames).map(basename => {
-      return `<option>${escapeHTML(basename)}</option>`
-    })
+  function render (posts) {
     response.end(`
 <!doctype html>
 <html lang=en-US>
@@ -136,72 +104,49 @@ function get (request, response) {
     <meta name=viewport content=width=device-width,initial-scale=1>
     <title>${escapeHTML(TITLE)}</title>
     <style>
-table {
-  border-collapse: collapse;
+.posts li {
+  margin: 1rem 0;
 }
 
-a[href] {
-  color:inherit;
-  text-decoration: none;
-}
-
-a[href]:hover {
-  text-decoration: underline;
-}
-
-.overdue {
-  color: darkred;
-}
-
-.today {
-  color: green;
-}
-
-input {
+.posts .description,
+.posts a[href],
+.posts date {
   display: block;
-  width: 100%;
-  margin: 0.5rem 0;
-  padding: 0.25rem;
-  box-sizing: border-box;
-}
-
-header, main {
-  max-width: 40rem;
-  margin: 1rem auto;
-}
-
-th, td {
-  padding: 0.25rem;
-  vertical-align: top;
 }
     </style>
   </head>
   <body>
+    <script>
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('click', event => {
+    const target = event.target
+    if (target.tagName !== 'BUTTON') return
+    const url = target.dataset.url
+    fetch("/?url=" + url, { method: 'POST' })
+      .then(response => {
+        if (response.status === 200) {
+          target.parentNode.removeChild(target)
+        } else {
+          window.alert('error marking read')
+        }
+      })
+  })
+})
+    </script>
     <header role=banner>
       <h1>${escapeHTML(TITLE)}</h1>
     </header>
     <main role=main>
-      <p>Last Updated: ${lastUpdated ? lastUpdated.fromNow() : ''}</p>
-      <form method=post action=${refreshPath}>
-        <input type=submit value="Refresh">
-      </form>
-      <h2>New</h2>
-      <form method=post>
-        <label for=basename>Client</label>
-        <input name=basename type=text list=basenames required>
-        <datalist id=basenames>${options}</datalist>
-        <label for=text>Text</label>
-        <input name=text type=text required>
-        <label for=Date>Date</label>
-        <input name=date type=date value=${moment().tz(TZ).format('YYYY-MM-DD')} required>
-        <input type=submit>
-      </form>
-      <h2>Today</h2>
-      ${renderTable(dueToday, false)}
-      <h2>Tasks</h2>
-      ${renderTable(withDueDate, true)}
-      <h2>Ongoing</h2>
-      ${renderLists(ongoing)}
+      <ul class=posts>
+        ${posts.map(item => `
+        <li>
+          <span class=description>${escapeHTML(item.description)}</span>
+          <a href="${escapeHTML(item.href)}">${escapeHTML(item.href)}</a>
+          <date>${escapeHTML(item.time)}</date>
+          <button data-url="${escapeHTML(item.href)}">Mark Read</button>
+        </li>
+        `).join('')}
+      </ul>
     </main>
   </body>
 </html>
@@ -209,98 +154,9 @@ th, td {
   }
 }
 
-function renderTable (todos, dateColumn) {
-  const todayMoment = moment()
-  return `
-  <table>
-    <tbody>
-      ${todos.map(row).join('')}
-    </tbody>
-  </table>
-  `.trim()
-
-  function row (todo) {
-    let status = ''
-    let dateString = todo.dateString || ''
-    const todoMoment = moment.tz(dateString, TZ)
-    if (dateString) {
-      if (todoMoment.isSame(todayMoment, 'day')) status = 'today'
-      else if (todoMoment.isBefore(todayMoment)) {
-        status = 'overdue'
-      }
-    }
-    if (dateString) {
-      if (todo.today) dateString = 'today'
-      else dateString = todoMoment.startOf('day').fromNow()
-    }
-    return `
-<tr class=${status}>
-  <td>${escapeHTML(todo.basename)}</td>
-  <td>${linkifyURLs(escapeHTML(lineToDisplay(todo)))}</td>
-  ${dateColumn ? `<td title="${todo.dateString}">${dateString}</td>` : ''}
-</tr>
-    `.trim()
-  }
-}
-
-function renderLists (todos) {
-  let basenames = new Set()
-  todos.forEach(todo => {
-    basenames.add(todo.basename)
-  })
-  basenames = Array.from(basenames)
-  return basenames
-    .map(basename => {
-      const subset = todos
-        .filter(todo => todo.basename === basename)
-        .sort((a, b) => {
-          return a.line.toLowerCase().localeCompare(b.line.toLowerCase())
-        })
-      return `
-      <h3 id="${escapeHTML(basename)}">${escapeHTML(basename)}</h3>
-      <ul>
-        ${subset.map((todo) => `<li>${lineToDisplay(todo)}</li>`).join('')}
-      </ul>
-      `.trim()
-    })
-    .join('')
-}
-
-function lineToDisplay (todo) {
-  return todo.line
-    .replace(dateRE, '')
-    .replace(continuingRE, '')
-}
-
-const dateRE = /(\d\d\d\d-\d\d-\d\d)/
-const continuingRE = /\.\.\./
-
-function processFile (basename, callback) {
-  const file = path.join(REPOSITORY, basename)
-  fs.readFile(file, 'utf8', (error, text) => {
-    if (error) return callback(error)
-    const lines = text
-      .split('\n')
-      .filter(element => !!element)
-    const results = []
-    lines.forEach(line => {
-      const dateMatch = dateRE.exec(line)
-      if (dateMatch) {
-        const dateString = dateMatch[1]
-        results.push({ dateString, line, basename })
-      }
-      const continuingMatch = continuingRE.exec(line)
-      if (continuingMatch) {
-        results.push({ line, continuing: true, basename })
-      }
-    })
-    callback(null, results)
-  })
-}
-
-const Busboy = require('busboy')
-const runSeries = require('run-series')
-const spawn = require('child_process').spawn
+const https = require('https')
+const querystring = require('querystring')
+const parseURL = require('url-parse')
 
 function post (request, response) {
   const auth = basicAuth(request)
@@ -309,123 +165,176 @@ function post (request, response) {
     response.setHeader('WWW-Authenticate', 'Basic realm=todo')
     return response.end()
   }
-  let basename, text, date
-  request.pipe(
-    new Busboy({ headers: request.headers })
-      .on('field', (name, value) => {
-        if (name === 'basename') {
-          basename = value.trim()
-        } else if (name === 'text') {
-          text = value.trim()
-        } else if (name === 'date') {
-          date = new Date(value)
-        }
-      })
-      .on('finish', () => {
-        request.log.info({ basename, text, date }, 'data')
-        const line = text + ' ' + dateToString(date)
-        const file = path.join(REPOSITORY, basename)
-        runSeries([
-          loggedTask('reset', done => {
-            resetToOriginMaster(done)
-          }),
-          loggedTask('append', done => {
-            fs.appendFile(file, '\n' + line + '\n', done)
-          }),
-          loggedTask('git add', done => {
-            spawnGit(['add', basename], done)
-          }),
-          loggedTask('git commit', done => {
-            spawnGit(['commit', '--allow-empty-message', '-m', ''], done)
-          }),
-          loggedTask('git push', done => {
-            spawnGit(['push'], done)
-          })
-        ], error => {
-          if (error) {
-            response.statusCode = 500
-            return response.end(error.message)
-          }
-          response.statusCode = 303
-          response.setHeader('Location', '/')
-          response.end()
-        })
-
-        function loggedTask (message, task) {
-          return done => {
-            task(error => {
-              request.log.info('start: ' + message)
-              if (error) return done(error)
-              request.log.info('end: ' + message)
-              done()
-            })
-          }
-        }
-      })
-  )
-}
-
-function refresh (request, response) {
-  resetToOriginMaster(error => {
+  const { url } = parseURL(request.url, true).query
+  if (!url) {
+    response.statusCode = 400
+    return response.end()
+  }
+  request.log.info({ url }, 'marking read')
+  markRead(request, url, error => {
     if (error) {
+      request.log.error(error)
       response.statusCode = 500
-      response.end(error.toString())
+      return response.end()
     }
-    response.statusCode = 303
-    response.setHeader('Location', '/')
     response.end()
   })
 }
 
-function spawnGit (args, callback) {
-  spawn('git', args, { cwd: REPOSITORY })
-    .once('close', code => {
-      if (code === 0) return callback()
-      const chunks = []
-      process.stderr
-        .on('data', chunk => { chunks.push(chunk) })
-        .once('end', () => {
-          const output = Buffer.concat(chunks).toString()
-          const description = `git ${args.join(' ')}`
-          callback(new Error(`${description} failed:\n` + output))
+function markRead (request, url, callback) {
+  const postProperties = {
+    href: 'url',
+    description: 'description',
+    extended: 'extended',
+    tags: 'tags',
+    time: 'dt',
+    shared: 'shared'
+  }
+  let oldPost
+  runSeries([
+    // Fetch existing post data.
+    done => {
+      https.get(`https://api.pinboard.in/v1/posts/get?url=${url}&format=json&auth_token=${PINBOARD_TOKEN}`)
+        .once('error', error => done(error))
+        .once('response', response => {
+          const chunks = []
+          response
+            .on('data', chunk => { chunks.push(chunk) })
+            .once('error', error => done(error))
+            .once('end', () => {
+              const buffer = Buffer.concat(chunks)
+              let parsed
+              try {
+                parsed = JSON.parse(buffer)
+              } catch (error) {
+                return done(error)
+              }
+              request.log.info(parsed, 'parsed')
+              if (!Array.isArray(parsed.posts)) {
+                return done(new Error('no posts array'))
+              }
+              const length = parsed.posts.length
+              if (length !== 1) {
+                return done(new Error(`${length} posts`))
+              }
+              oldPost = parsed.posts[0]
+              done()
+            })
         })
-    })
+    },
+    // Overwrite the post.
+    done => {
+      const query = {
+        format: 'json',
+        auth_token: PINBOARD_TOKEN,
+        toread: 'no'
+      }
+      for (const [from, to] of Object.entries(postProperties)) {
+        query[to] = oldPost[from]
+      }
+      request.log.info(query, 'query')
+      https.get(`https://api.pinboard.in/v1/posts/add?${querystring.stringify(query)}`)
+        .once('error', error => done(error))
+        .once('response', response => {
+          const { statusCode } = response
+          if (statusCode !== 200) {
+            return done(new Error('pinboard responded ' + statusCode))
+          }
+          const chunks = []
+          response
+            .on('data', chunk => { chunks.push(chunk) })
+            .once('error', error => done(error))
+            .once('end', () => {
+              const buffer = Buffer.concat(chunks)
+              let parsed
+              try {
+                parsed = JSON.parse(buffer)
+              } catch (error) {
+                return done(error)
+              }
+              request.log.info(parsed, 'pinboard response')
+              done()
+            })
+        })
+    }
+  ], callback)
 }
 
-function resetToOriginMaster (callback) {
-  runSeries([
-    done => { spawnGit(['fetch', 'origin'], done) },
-    done => { spawnGit(['reset', '--hard', 'origin/master'], done) }
-  ], error => {
-    if (error) return callback(error)
-    lastUpdated = moment().tz(TZ)
-    callback()
-  })
-}
+const runSeries = require('run-series')
 
 server.listen(process.env.PORT || 8080, function () {
   const port = this.address().port
   log.info({ port }, 'listening')
 })
 
-resetToOriginMaster(() => { log.info('reset') })
-
+const { pipeline } = require('stream')
 const schedule = require('node-schedule')
 const EVERY_TEN_MINUTES = '*/10 * * * *'
-schedule.scheduleJob(EVERY_TEN_MINUTES, () => {
-  resetToOriginMaster(error => {
+schedule.scheduleJob(EVERY_TEN_MINUTES, fetchPosts)
+
+function fetchPosts () {
+  runParallel({
+    // Read last updated date from disk.
+    disk: done => {
+      fs.readFile(UPDATED_FILE, 'utf8', (error, date) => {
+        if (error) {
+          if (error.code === 'ENOENT') return done(null, null)
+          return done(error)
+        }
+        date = date.trim()
+        log.info({ date }, 'disk')
+        return done(null, date)
+      })
+    },
+    // Fetch last updated date from Pinboard.
+    api: done => {
+      https.get(`https://api.pinboard.in/v1/posts/update?format=json&auth_token=${PINBOARD_TOKEN}`)
+        .once('error', error => { done(error) })
+        .once('response', response => {
+          const chunks = []
+          response
+            .on('data', chunk => { chunks.push(chunk) })
+            .once('error', error => done(error))
+            .once('end', () => {
+              const buffer = Buffer.concat(chunks)
+              let parsed
+              try {
+                parsed = JSON.parse(buffer)
+              } catch (error) {
+                return done(error)
+              }
+              const date = parsed.update_time.trim()
+              log.info({ date }, 'API')
+              done(null, date)
+            })
+        })
+    }
+  }, (error, { disk, api }) => {
     if (error) return log.error(error)
+    if (disk === api) return
+    runSeries([
+      done => {
+        log.info('fetching posts')
+        https.get(`https://api.pinboard.in/v1/posts/all?format=json&auth_token=${PINBOARD_TOKEN}`)
+          .once('error', error => done(error))
+          .once('response', response => {
+            log.info('writing to disk')
+            pipeline(
+              response,
+              fs.createWriteStream(POSTS_FILE),
+              error => {
+                if (error) return done(error)
+                log.info('wrote to disk')
+                done()
+              }
+            )
+          })
+      },
+      done => fs.writeFile(UPDATED_FILE, api, done)
+    ], error => {
+      if (error) return log.error(error)
+    })
   })
-})
-
-function dateToString (date) {
-  return (
-    date.getFullYear() + '-' +
-    (date.getMonth() + 1).toString().padStart(2, '0') + '-' +
-    date.getDate().toString().padStart(2, '0')
-  )
 }
 
-function compareDateStrings (a, b) {
-  return a.trim().localeCompare(b.trim())
-}
+fetchPosts()
