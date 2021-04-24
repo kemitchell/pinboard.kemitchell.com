b53e8f759bd850d8751494ce1a3719c3082d506b
Kyle E. Mitchell <kyle@kemitchell.com>
2021-04-23 20:32:27 -0700

    Add code comments

--- client.js
+++ client.js
@@ -1,13 +1,17 @@
+/* eslint-env browser */
 document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('click', event => {
     const target = event.target
     if (target.tagName !== 'BUTTON') return
+    // Find the URL of the post.
     const postURL = target.dataset.url
     const endpoint = new URL('/')
     endpoint.searchParams.append('url', postURL)
+    // Send POST.
     fetch(endpoint, { method: 'POST' })
       .then(response => {
         if (response.status === 200) {
+          // Remove list item for read post.
           const li = target.parentNode
           li.parentNode.removeChild(li)
         } else {
